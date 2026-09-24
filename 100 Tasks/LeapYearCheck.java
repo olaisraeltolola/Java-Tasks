1096bce7e0157693e10c562b39e3ba4cd11751b3
olaisraeltolola <olaisraelshyne@gmail.com>
2026-09-24 06:03:22 +0100

    Update

--- 100 Tasks/LeapYearCheck.java
+++ 100 Tasks/LeapYearCheck.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 System.out.println("Enter a year");
 int currentYear = input.nextInt();
 
-if (currentYear % 4 == 0 && currentYear % 400 == 0)
+if ((currentYear % 4 == 0 && currentYear % 100 != 0)|| currentYear % 400 == 0)
 	System.out.println("Leap year");
 else
 	System.out.println("It is not a leap year");
